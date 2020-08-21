613fe8b41dacc8f8a23c7f838464994d3093464f
Alvaro <dvagaymer@icloud.com>
2020-08-21 12:20:01 +0200

    added isspace

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: alopez-g <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/25 08:06:08 by alopez-g          #+#    #+#             */
-/*   Updated: 2020/07/31 15:44:57 by alopez-g         ###   ########.fr       */
+/*   Updated: 2020/08/21 12:19:31 by dvagaymer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t				ft_strlen(const char *s);
 int					ft_toupper(int c);
+int					ft_isspace(int c);
 int					ft_tolower(int c);
 int					ft_isprint(int c);
 int					ft_isascii(int c);
